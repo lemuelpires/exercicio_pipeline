d9693deaf1a55a8f09dec62743e13377d6fb1872
lemuelpires <lemuelpires@gmail.com>
2024-03-07 12:20:47 -0300

    atualizacao index.js

--- src/index.js
+++ src/index.js
@@ -1,23 +1,5 @@
-/*function multiplicacao(a, b) {
+function multiplicacao(a, b) {
   return a * b;
 }
 
-module.exports = multiplicacao;*/
-function multiplicacao() {
-  // Obter os valores dos números de entrada
-  var a = document.getElementById('a').value;
-  var b = document.getElementById('a').value;
-
-  // Verificar se ambos os campos foram preenchidos
-  if (a === '' || b === '') {
-      alert('Por favor, insira ambos os números.');
-      return;
-  }
-
-  // Calcular a multiplicação
-  var resultado = parseInt(a) * parseInt(b);
-
-  // Exibir o resultado
-  document.getElementById('resultado').innerText = 'Resultado: ' + resultado;
-}
 module.exports = multiplicacao;

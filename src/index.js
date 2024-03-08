7e884fd2d6d0479743e259cda22e66b6c28143c8
lemuelpires <lemuelpires@gmail.com>
2024-03-08 09:39:46 -0300

    atualizacao index

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
+/*function multiplicacao(a, b) {
+  return a * b;
+}
+module.exports = multiplicacao;*/
 function multiplicacao(a, b) {
   return a * b;
 }
 
-module.exports = multiplicacao;
+// Verifica se está no ambiente de teste (Node.js com Jest)
+if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+  module.exports = multiplicacao;
+}

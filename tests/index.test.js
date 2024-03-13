99677b4352a2bb970fb87bb46cb95e57e1d557e5
lemuelpires <lemuelpires@gmail.com>
2024-03-13 09:08:22 -0300

    alteracao numeros multiplicacao

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,13 +1,3 @@
-// index.test.js
-/*const multiplicacao = require('../src/index.js');
-
-describe('Multiplicação', () => {
-  test('deve multiplicar dois números corretamente', () => {
-    expect(multiplicacao(2, 3)).toBe(6);
-    expect(multiplicacao(5, 5)).toBe(25);
-    expect(multiplicacao(0, 10)).toBe(0);
-  });
-});*/
 const multiplicacao = require('../src/index.js');
 
 describe('Multiplicação', () => {
